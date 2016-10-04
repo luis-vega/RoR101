781e45bc1abe18b213c81b8e8f0533dd84745cf7
Luis Vega <luisvega29@gmail.com>
2016-10-04 09:00:12 -0500

    Searchable method commented

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ActiveRecord::Base
 	mount_uploader :image,ImageUploader 
 
-	searchable do 
-		text :name, :age, :email, :born_date,:city 
-	end
+	#searchable do 
+	#	text :name, :age, :email, :born_date,:city 
+	#end
 end
